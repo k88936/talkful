a89251073e052b289b1fca7fe070949b06ac8c08
kvtodev <kvtodev@outlook.com>
2026-04-09 14:41:57 +0800

    no resample, sherpa do it  internally

--- src-tauri/src/asr/mod.rs
+++ src-tauri/src/asr/mod.rs
@@ -2,39 +2,8 @@ pub mod sherpa_asr_service;
 
 use crate::record::RecordResult;
 use anyhow::{Error, Result};
-use rubato::audioadapter_buffers::direct::InterleavedSlice;
-use rubato::{Async, FixedAsync, PolynomialDegree, Resampler};
-use std::result;
 
 pub trait ASRService {
     const TARGET_SAMPLE_RATE_HZ: u32 = 16_000;
     fn asr(&mut self, sample: RecordResult) -> Result<String, Error>;
-}
-
-fn resample_audio(audio: &[f32], from_rate: u32, to_rate: u32) -> result::Result<Vec<f32>, Error> {
-    if from_rate == to_rate {
-        return Ok(audio.to_vec());
-    }
-
-    let ratio = to_rate as f64 / from_rate as f64;
-    let chunk_size = 1024;
-
-    let mut resampler = Async::<f32>::new_poly(
-        ratio,
-        1.0,
-        PolynomialDegree::Septic,
-        chunk_size,
-        1,
-        FixedAsync::Input,
-    )?;
-
-    let input = InterleavedSlice::new(audio, 1, audio.len())?;
-    let mut output = vec![0.0f32; resampler.process_all_needed_output_len(audio.len())];
-    let output_frames = output.len();
-    let mut output_adapter = InterleavedSlice::new_mut(&mut output, 1, output_frames)?;
-    let (_input_frames, output_frames) =
-        resampler.process_all_into_buffer(&input, &mut output_adapter, audio.len(), None)?;
-    output.truncate(output_frames);
-
-    Ok(output)
 }
--- src-tauri/src/asr/sherpa_asr_service.rs
+++ src-tauri/src/asr/sherpa_asr_service.rs
@@ -1,14 +1,13 @@
-use sherpa_rs::paraformer::{ParaformerConfig, ParaformerRecognizer};
-use anyhow::Error;
-use crate::asr;
 use crate::asr::ASRService;
 use crate::record::RecordResult;
+use anyhow::Error;
+use sherpa_rs::paraformer::{ParaformerConfig, ParaformerRecognizer};
 
 pub struct SherpaASRService {
     recognizer: ParaformerRecognizer,
 }
 
-impl SherpaASRService{
+impl SherpaASRService {
     pub fn new(model: Option<&str>, tokens: Option<&str>) -> anyhow::Result<Self> {
         let config = ParaformerConfig {
             model: model.unwrap_or("models/asr/model.int8.onnx").into(),
@@ -19,21 +18,13 @@ impl SherpaASRService{
         let recognizer = ParaformerRecognizer::new(config).map_err(Error::msg)?;
         Ok(Self { recognizer })
     }
-
 }
 
 impl ASRService for SherpaASRService {
-    const TARGET_SAMPLE_RATE_HZ: u32 = 16_000;
     fn asr(&mut self, sample: RecordResult) -> anyhow::Result<String, Error> {
-        let resampled = asr::resample_audio(
-            &(&sample.samples),
-            sample.sample_rate,
-            Self::TARGET_SAMPLE_RATE_HZ,
-        )
-        .expect("resample failed");
         Ok(self
             .recognizer
-            .transcribe(Self::TARGET_SAMPLE_RATE_HZ, &resampled)
+            .transcribe(sample.sample_rate, &sample.samples)
             .text)
     }
 }
